89112f5fa0767c32de27b1b4d6b15261e4f683c1
chick_0 <chick_0@ch1ck.xyz>
2022-09-02 16:20:36 +0900

    update image click handler * size up and down

--- list-of-cat-images/index.js
+++ list-of-cat-images/index.js
@@ -4,7 +4,7 @@ import { registerLink } from "../share/link.js";
 const flex = document.querySelector("section.flex");
 
 /**
- * Render 'List of cat images' display
+ * Render 'List of cat images' page
  */
 function render() {
     flex.innerHTML = "";
@@ -13,7 +13,38 @@ function render() {
         img.src = path;
 
         img.addEventListener('click', (event) => {
-            window.open(event.target.src);
+            /**
+             * Scroll to clicked image
+             */
+            function scroll() {
+                event.target.scrollIntoView({
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center"
+                });
+            }
+
+            if(document.querySelectorAll("img.clicked").length == 0) {
+                event.target.classList.add("clicked");
+                scroll();
+            } else {
+                let flag_add_clicked = true;
+
+                document.querySelectorAll("img.clicked").forEach((element) => {
+                    if(element === event.target) {
+                        flag_add_clicked = false;
+                    }
+
+                    element.classList.remove("clicked");
+                });
+
+                if(flag_add_clicked === true) {
+                    event.target.classList.add("clicked");
+                    scroll();
+                } else {
+                    scroll();
+                }
+            }
         });
 
         flex.appendChild(img);
